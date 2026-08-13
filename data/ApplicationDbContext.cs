22772de30f95aa25108c9dc33f44adc095fd0d3f
didi-creator <148676687+didi-creator@users.noreply.github.com>
2026-08-13 12:50:28 +0300

    Add ApplicationDbContext for database context

---
+++ data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using FacultyInformationSystem_FIS_.Models;
+
+namespace FacultyInformationSystem_FIS_.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoRequest> DemoRequests => Set<DemoRequest>();
+    }
+}
