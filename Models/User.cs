3d6589bef24062eaf197995ffa5d6f9fecd01b68
didi-creator <148676687+didi-creator@users.noreply.github.com>
2026-08-20 15:01:24 +0300

    Refactor User class and add UserRole class
    Updated User class to inherit from BaseEntity and added UserRole class for role management.

--- Models/User.cs
+++ Models/User.cs
@@ -2,10 +2,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FacultyInformationSystem_FIS_.Models
 {
-    public class User
+    public class User : BaseEntity
     {
-        public int Id { get; set; }
-
         [Required, MaxLength(200)]
         public string FullName { get; set; } = "";
 
@@ -18,5 +16,14 @@ namespace FacultyInformationSystem_FIS_.Models
         public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public class UserRole : BaseEntity // User roles, e.g. user can be faculty, admin, department chair, etc.
+    {
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
     }
 }
