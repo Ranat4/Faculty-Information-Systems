cacb22483e8d3ef97e08d5a313c9ebe025578f40
didi-creator <148676687+didi-creator@users.noreply.github.com>
2026-09-02 10:57:02 +0300

    Add files via upload

---
+++ Models/DocumentStatus.cs
@@ -0,0 +1,11 @@
+namespace FacultyInformationSystem_FIS_.Models
+{
+    public enum DocumentStatus
+    {
+        Draft,
+        PendingReview,
+        SentBackToSubmitter,
+        Rejected,
+        Reviewed
+    }
+}
